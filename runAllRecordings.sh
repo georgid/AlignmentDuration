f1d6cfc6a416aac10b7e2d1ed65a5bf86f7ab103
georgid <georgi.dzhambazov@upf.edu>
2015-01-13 15:15:31 +0200

    added runallRecordings.sh

---
+++ runAllRecordings.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$ -q default.q
+#$ -pe smp 8
+#$ -l s_vmem=8G
+#$ -l h_vmem=10G
+#$ -m bea
+#$ -M georgi.dzhambazov@upf.edu
+#$ -e allRecordingsParamsError.out
+#$ -o allRecordingsParamsOutput.out
+#$ -cwd
+#$ -N allRecordingsParams
+
+# A SCRIPT THAT RUNS on HPC
+
+module load python/2.7.5
+echo "python after loading module: "
+which python
+source /homedtic/georgid/env/bin/activate
+echo "python after loading vrit env: "
+which python
+
+
+HOME_DTIC=/homedtic/georgid/
+
+
+#$HOME_DTIC/env/bin/python AlignmentDuration/doitAllRecordings.py /homedtic/georgid/turkish-makam-lyrics-2-audio-test-data/  /homedtic/georgid/ISTANBUL/ True
+$HOME_DTIC/env/bin/python AlignmentDuration/doitAllRecordings.py $HOME_DTIC//turkish-makam-lyrics-2-audio-test-data-synthesis/ $HOME_DTIC//turkish-makam-lyrics-2-audio-test-data-synthesis/  0.97 False True
