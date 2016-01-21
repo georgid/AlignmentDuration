085d5a7580bccc136146f175c172153ec12d371d
georgid <georgi.dzhambazov@upf.edu>
2016-01-21 18:49:16 +0100

    README

--- setup.py
+++ setup.py
@@ -7,6 +7,45 @@ Created on Jan 15, 2016
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from setuptools import Extension
+import os
+import numpy as np
+
+
+
+
+try:
+   from distutils.command.build_py import build_py_2to3 \
+       as build_py
+except ImportError:
+   from distutils.command.build_py import build_py
+   
+try:
+   from Cython.Distutils import build_ext
+except ImportError:
+   use_cython = False
+else:
+   use_cython = True
+   
+
+# py_inc = [get_python_inc()]
+# 
+# np_lib = os.path.dirname(numpy.__file__)
+# np_inc = [os.path.join(np_lib, 'core/include')]
+# ext_inc = os
+
+pathSmsTools = 'smstools/software/models/utilFunctions_C/'
+sourcefiles = [pathSmsTools + 'utilFunctions.c', pathSmsTools + 'cutilFunctions.pyx']
+ext_modules = [Extension("smstools.software.models.utilFunctions_C",
+            sourcefiles,
+            libraries=['m'],
+            include_dirs=[np.get_include()]
+            )]
+
+generalDependencies = ['setuptools-git', 'numpy', 'cython']
+# dependencies of hmm package dir
+hmmDependencies = ['scipy', 'matplotlib']
+generalDependencies.extend(hmmDependencies)
 
 setup(name='lyrics-align',
       version='0.1',
@@ -16,7 +55,11 @@ setup(name='lyrics-align',
       # packages=['align', 'align.model' ,'hmm', 'hmm.continuous',  'test']
     packages=find_packages(),
     package_data={'align': ['model/hmmdefs9gmm9iter']},
-    include_package_data=False,
-    install_requires=['setuptools-git']
+    include_package_data=True,
+    install_requires=generalDependencies,
+    
+    # cython dependencies
+    cmdclass = {'build_ext': build_ext},
+    ext_modules=ext_modules
 
 )
