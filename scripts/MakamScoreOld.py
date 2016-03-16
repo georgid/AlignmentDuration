c26b91a8e6b0bc3be7dc4bc036a7c8805383c636
georgid <georgi.dzhambazov@upf.edu>
2016-03-16 22:18:29 +0100

    added code for extending old audio annotations of sections with only names to new section name symbols C1, B2 etc.

---
+++ scripts/MakamScoreOld.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+'''
+contains a class
+Created on Mar 3, 2014
+
+@author: joro
+'''
+
+
+import os
+import sys
+import imp
+from align.Phonetizer import Phonetizer
+from align.Decoder import logger
+
+# trick to make terminal NOT assume ascii
+reload(sys).setdefaultencoding("utf-8")
+
+parentDir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(sys.argv[0]) ), os.path.pardir)) 
+pathUtils = os.path.join(parentDir, 'utilsLyrics')
+
+sys.path.append(pathUtils )
+
+from utilsLyrics.Utilz import writeListToTextFile, findFileByExtensions
+import codecs
+
+import glob
+from SymbTrParserOld import SymbTrParserOld
+# from utils.Utils import writeListToTextFile
+
+
+# 
+# COMPOSITION_NAME = 'muhayyerkurdi--sarki--duyek--ruzgar_soyluyor--sekip_ayhan_ozisik'
+# COMPOSITION_NAME = 'huseyni--sarki--turkaksagi--hicran_oku--sevki_bey'
+# 
+# PATH_TEST_DATASET='/Users/joro/Documents/Phd/UPF/turkish-makam-lyrics-2-audio-test-data/'
+# PATH_TEST_DATASET = '/Volumes/IZOTOPE/sertan_sarki/'
+
+class MakamScoreOld():
+    '''
+    classdocs
+    '''
+
+
+##################################################################################
+
+    def __init__(self, pathToSymbTrFile, pathToSectionTsvFile):
+        '''
+        Constructor
+        
+        '''
+        self.compositionName = os.path.splitext(pathToSymbTrFile)[0]
+        
+        ''' lyrics divided into sectons.
+        # e.g. "nakarat" : [ word1 word2 ] '''
+        self.sectionToLyricsMap = []
+        
+        self._loadSectionsAndSyllablesFromSymbTr(pathToSymbTrFile, pathToSectionTsvFile)
+        
+        
+        # pats to individual ..txt lyrics files. 
+        self.pathsTolyricSectionFiles = []
+        
+      
+  ##################################################################################
+    '''
+    parses symbTr file. Reads lyrics, 
+    reads section names
+    groups together section names and lyrics 
+    '''
+    def _loadSectionsAndSyllablesFromSymbTr(self, pathToSymbTrFile, pathToSectionTsvFile):
+        symbTrParser = SymbTrParserOld(pathToSymbTrFile, pathToSectionTsvFile)
+       
+        # list of Word object
+        symbTrParser.syllables2Lyrics()
+        lyricsAllSections = symbTrParser.sectionLyrics
+        
+        # for each section part
+        for currSectionBoundary,currSectionLyrics in zip(symbTrParser.sectionboundaries, lyricsAllSections):
+            # setionName, melodicSTRucture, lyricStructure, lyrics
+            quadrupleSectionNameAndLyrics =  currSectionBoundary[0], currSectionBoundary[1], currSectionBoundary[2],  currSectionLyrics  
+            self.sectionToLyricsMap.append(quadrupleSectionNameAndLyrics)
+            
+    def getLyricsForSection(self,sectionNumber):
+        '''
+        convenience getter
+        '''
+        
+        if sectionNumber >=  len(self.sectionToLyricsMap) or sectionNumber < 0:
+            sys.exit("section withNumber {} not present in score. Check your .sections.tsv file".format(sectionNumber) )
+
+        #python indexing starts from zero
+        lyrics = self.sectionToLyricsMap[sectionNumber][3]
+        if not lyrics.listWords:
+            logger.warn("no lyrics for demanded section {} : {}".format(sectionNumber, self.sectionToLyricsMap[sectionNumber][0] ))
+            return None
+        return lyrics 
+ 
+  
+   ##################################################################################
+    def printSectionsAndLyrics(self):
+        '''
+        utility method to print all lyrics that are read from symbTr
+        '''
+        for currSection in self.sectionToLyricsMap:
+    
+            print '\n' + str(currSection[0])  + " " + str(currSection[1]) +  " " + str(currSection[2]) + ':'
+
+            print currSection[3]
+#             for word in  currSection[1]:
+#                 print word.__str__().encode('utf-8','replace')
+        
+
+    def serializePhonemesForSection(self, whichSection, outputFileName):
+        '''
+        list of all phonemes. print to file @param outputFileName
+        '''    
+        lyrics = self.getLyricsForSection(whichSection)
+        if not lyrics:
+            sys.exit("no lyrics")
+        
+        writeListToTextFile(lyrics.phonemesNetwork, None,  outputFileName )
+        return lyrics.phonemesNetwork
+    
+            
+        
+        
+    def printSyllables(self, whichSection):
+        '''
+        debug: print syllables 
+        '''
+        
+        lyrics = self.getLyricsForSection(whichSection)
+        if not lyrics:
+            sys.exit("no lyrics")
+            
+        lyrics.printSyllables
+        
+     
+               
+
+
+def loadLyrics(pathToComposition, whichSection):
+
+    Phonetizer.initLookupTable(False,  'grapheme2METUphonemeLookupTable')
+
+    
+    os.chdir(pathToComposition)
+
+    pathTotxt = os.path.join(pathToComposition, glob.glob("*.txt")[0])
+    
+    listExtensions = [ "sectionsMetadata_symbTr1.json", "sections.tsv", "sections.txt"]
+    sectionFiles = findFileByExtensions(pathToComposition, listExtensions)
+    sectionFile = sectionFiles[0]
+        
+    pathToSectionTsv = os.path.join(pathToComposition, sectionFile)
+    makamScore = MakamScoreOld(pathTotxt, pathToSectionTsv )
+    
+    # phoneme IDs
+    
+    lyrics = makamScore.getLyricsForSection(whichSection)
+    return lyrics
+
+def loadMakamScore(pathToComposition):
+    '''
+    same as loadLyrics, but return MakamScoreOld, so that all lyrics can be shown if needed
+    '''
+    Phonetizer.initLookupTable(False,  'grapheme2METUphonemeLookupTable')
+    
+    os.chdir(pathToComposition)
+
+    pathTotxt = os.path.join(pathToComposition, glob.glob("*.txt")[0])
+    
+    listExtensions = [ "sections.json", "sections.tsv", "sections.txt"]
+    sectionFiles = findFileByExtensions(pathToComposition, listExtensions)
+    sectionFile = sectionFiles[0]
+        
+    pathToSectionTsv = os.path.join(pathToComposition, sectionFile)
+    makamScore = MakamScoreOld(pathTotxt, pathToSectionTsv )
+    return makamScore
+    
+
+def testMakamScore(argv):
+        if len(argv) != 2:
+            print ("usage: {} <path to symbtTr.txt and symbTr.sections.tsv>".format(argv[0]) )
+            sys.exit();
+        pathToComposition = argv[1]
+        
+        makamScore = loadMakamScore(pathToComposition)
+        makamScore.printSectionsAndLyrics()
+#         lyrics = loadLyrics(pathToComposition, whichSection=0)
+     
+      
+
+          
+             
+if __name__ == '__main__':
+
+        # only for unit testing purposes
+        
+        print "in Makam Score"
+        a = ['dummy', '/Users/joro/Documents/Phd/UPF/turkish-makam-lyrics-2-audio-test-data-synthesis/ussak--sarki--aksak--bu_aksam_gun--tatyos_efendi/']
+        
+        
+        a = ['dummy', '/Users/joro/Documents/Phd/UPF/turkish-makam-lyrics-2-audio-test-data-synthesis/rast--turku--semai--gul_agaci--necip_mirkelamoglu/']
+        
+        a = ['dummy', '/Users/joro/Documents/Phd/UPF/turkish-makam-lyrics-2-audio-test-data-synthesis/nihavent--sarki--duyek--bir_ihtimal--osman_nihat_akin/']
+        
+        a = ['dummy', '/Users/joro/Documents/Phd/UPF/turkish-makam-lyrics-2-audio-test-data-synthesis/segah--sarki--curcuna--olmaz_ilac--haci_arif_bey/']
+        
+        a = ['dummy', '/Users/joro/Documents/Phd/UPF/turkish-makam-lyrics-2-audio-test-data-synthesis/rast--sarki--curcuna--nihansin_dideden--haci_faik_bey/']
+        a = ['dummy', '/Users/joro/Documents/Phd/UPF/turkish-makam-lyrics-2-audio-test-data-synthesis/rast--sarki--sofyan--gelmez_oldu--dramali_hasan/']
+        a = ['dummy', '/Users/joro/Documents/Phd/UPF/turkish-makam-lyrics-2-audio-test-data-synthesis/rast--turku--semai--gul_agaci--necip_mirkelamoglu/']
+
+        
+        testMakamScore(a)
+          
+      
+         
---
+++ scripts/extendSectionLinksNewMetadata.py
@@ -0,0 +1,153 @@
+'''
+Created on Feb 11, 2016
+
+@author: joro
+'''
+import json
+import sys
+from align.ScoreSection import ScoreSection
+from align.MakamRecordingOld import MakamRecordingOld
+from scripts.MakamScoreOld import loadMakamScore
+import os
+# from align.RecordingSegmenter import getURISectionAnnotation
+
+parentDir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(sys.argv[0]) ), os.path.pardir, os.path.pardir)) 
+pathUtils = os.path.join(parentDir, 'pycompmusic') 
+if pathUtils not in sys.path:
+    sys.path.append(pathUtils)
+
+import compmusic    
+from compmusic import dunya
+from compmusic.extractors.makam.lyricsalignLocal import recMBIDs, recordingDirs
+# from align.MakamRecordingOld import MakamRecordingOld
+
+from utilsLyrics.Utilz import  findFileByExtensions
+
+pathSectionAnnosSource = '/Users/joro/Downloads/turkish_makam_section_dataset-2014_jnmr/audio_metadata/'
+
+pathDestinationDataset = '/Users/joro/Downloads/lyrics-2-audio-test-data/'
+
+URI_datasetOld = '/Users/joro/Downloads/turkish-makam-lyrics-2-audio-test-data-synthesis/'
+
+
+
+dunya.set_token("69ed3d824c4c41f59f0bc853f696a7dd80707779")
+
+
+
+def extendNewMetadata(musicbrainzid, recordingDir):
+    '''
+    annotations with score sections names zemin meyan > convert to > annotations with sections names C1, B1. etc
+    uses old makamScore and makamRecording (and SymbTrParser old ) because we need the matchSections and its neater to use it form the constructor of MakamRecording
+    '''
+    
+    rec_data = dunya.makam.get_recording(musicbrainzid )
+    w = rec_data['works'][0]
+
+    
+    ###### 1. load sections with meyan etc. names and section annotation in json and match
+    symbtrtxtURI, symbTrCompositionName  = constructSymbTrTxtURI(URI_datasetOld, w['mbid'])
+    compositionPath = URI_datasetOld + symbTrCompositionName + '/'
+    makamScore = loadMakamScore(compositionPath)
+    makamScore.printSectionsAndLyrics()
+    pathToAudioFile = 'blah'
+    
+    
+    pathToRecording, pathToSectionAnnotations = getURISectionAnnotation(recordingDir, compositionPath) 
+    makamRecording = MakamRecordingOld(makamScore, pathToAudioFile, pathToSectionAnnotations)
+    print makamRecording.sectionIndices
+    
+    
+    ###### 2. load score sections metadata new C1,C2 etc.
+    sectionMetadata = dunya.docserver.get_document_as_json(w['mbid'], "metadata", "metadata", 1, version="0.1")
+
+    scoreSectionAnnos = sectionMetadata['sections']
+    if len(scoreSectionAnnos) != len(makamScore.sectionToLyricsMap):
+        sys.exit("text score sections with len {} and scoreSectionAnnos with len {}".format(len(makamScore.sectionToLyricsMap), len(scoreSectionAnnos) ) ) 
+    
+    scoreSections = []
+    for section in scoreSectionAnnos:
+                    print section
+                    sectionNew = ScoreSection(section['name'],  int(section['startNote']), int(section['endNote']), section['melodicStructure'], section['lyricStructure']) 
+                    scoreSections.append(sectionNew)
+    
+    ######### 3.  old format section annos in json
+#     sectionAnnosSourceURI = pathSectionAnnosSource + musicbrainzid + '.json'
+#     with open(sectionAnnosSourceURI) as f:
+#         sectionAnnosDict = json.load(f)
+#     if 'section_annotations' not in sectionAnnosDict:
+#                 sys.exit('annotation should have key section_annotations')
+#                 
+#     sectionAnnosJson = sectionAnnosDict['section_annotations']
+#     if len(sectionAnnosJson) != len(makamRecording.sectionIndices):
+#         sys.exit(" annotation local in \n {} \n has length  {} whereas annotation json \n {} \n has length {}. Rewrite code with only json".format(pathToSectionAnnotations, len(makamRecording.sectionIndices), sectionAnnosSourceURI, len(sectionAnnosJson)))
+#     
+#     for sectionAnnoTxt, sectionIdx in zip(sectionAnnosJson, makamRecording.sectionIndices):
+#         if sectionIdx != -1:  
+#             if sectionIdx > len(scoreSections): # done for        '06_Semahat_Ozdenses_-_Aksam_Oldu_Huzunlendim',
+#                 sys.exit('more sections matched that they are in sectiona metadata' )
+#             melStruct = scoreSections[sectionIdx].melodicStructure
+#             lyricStruct =   scoreSections[sectionIdx].lyricStructure
+#         
+#         else:
+#             melStruct = ''
+#             lyricStruct = ''
+#         sectionAnnoTxt['melodicStructure'] = melStruct
+#         sectionAnnoTxt['lyricStructure'] = lyricStruct
+#         
+#     sectionAnnosDict['section_annotations'] = sectionAnnosJson
+        
+    # if no section annos in pathSectionAnnosSource:
+    sections = []
+    for i, idx in enumerate(makamRecording.sectionIndices):
+        currSection = {}
+        currSection['time'] = [makamRecording.beginTs[i], makamRecording.endTs[i]]
+        currSection['time_unit'] = 'sec'
+        currSection['name'] = makamRecording.sectionNamesSequence[i]
+        if idx != -1: 
+            melStruct = scoreSections[idx].melodicStructure
+            lyrStruct = scoreSections[idx].lyricStructure
+        else:
+            melStruct = ''
+            lyrStruct = ''
+        currSection['melodicStructure'] = melStruct
+        currSection['lyricStructure'] = lyrStruct
+        sections.append(currSection)
+    sectionAnnosDict = {}
+    sectionAnnosDict['section_annotations'] = sections
+    
+    sectionAnnosURIEdited = pathDestinationDataset + musicbrainzid + '/' + musicbrainzid  + '.json'
+    print "wirting file \n" + sectionAnnosURIEdited
+    # write edited sectionAnnos
+    with open(sectionAnnosURIEdited, 'w') as f8:
+        json.dump( sectionAnnosDict, f8, indent=4)
+    
+    
+def constructSymbTrTxtURI(URI_dataset, workMBID):
+    '''
+    URI on local machine of symbTr queried by workMBID 
+    '''
+    symbtr = compmusic.dunya.makam.get_symbtr(workMBID)
+    symbTrCompositionName = symbtr['name']
+    
+    compositionPath = URI_dataset + symbTrCompositionName + '/'
+    symbtrtxtURI = compositionPath + symbTrCompositionName + '.txt'
+    
+    return symbtrtxtURI,  symbTrCompositionName
+
+
+def getURISectionAnnotation(recordingDir, pathToComposition):
+    pathToRecording = os.path.join(pathToComposition, recordingDir)
+    os.chdir(pathToRecording)
+#         pathToSectionAnnotations = os.path.join(pathToRecording, glob.glob('*.sectionAnno.txt')[0]) #             pathToAudio =  os.path.join(pathToRecording, glob.glob('*.wav')[0])
+    listExtensions = ["sectionAnno.json", "sectionAnno.txt", "sectionAnno.tsv"]
+    sectionAnnoFiles = findFileByExtensions(pathToRecording, listExtensions)
+    pathToSectionAnnotations = os.path.join(pathToRecording, sectionAnnoFiles[0])
+    return pathToRecording, pathToSectionAnnotations
+
+
+if __name__ == '__main__':
+    
+    for musicbrainzid, recordingDir in zip(recMBIDs, recordingDirs):
+         extendNewMetadata(musicbrainzid, recordingDir )
+         raw_input("press for next piece...")
