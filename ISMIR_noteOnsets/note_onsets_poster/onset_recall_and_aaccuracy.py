321cb70c148b3925844091b91ce3e7b91b30328f
georgid <georgi.dzhambazov@upf.edu>
2016-10-10 16:29:51 +0200

    report

---
+++ ISMIR_noteOnsets/note_onsets_poster/onset_recall_and_aaccuracy.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import matplotlib.pyplot as plt
+cF = [5,	4.5,	4.0,	3.5,	3.0]
+
+fig, ax1 = plt.subplots()
+
+plt.plot(cF, [57.2,	59.7,	66.8,	72.3,	73.2], 'ro-') # acapella onset recal
+plt.plot(cF, [52.8,	58.2,	65.9,	66.2,	68.4,], 'rs-') # poly onset recall
+
+
+ax1.axis([2.8, 5.2, 50, 80])
+plt.xlabel('cF')
+ax1.set_ylabel('onset recall', color='r')
+
+ax2 = ax1.twinx()
+ax2.axis([2.8, 5.2, 50, 80])
+ax2.set_ylabel('alignment accuracy', color='b')
+ax2.plot(cF, [71.1,	73.3,	74.5,	75.7,	72.0], 'bx-') # a cappella  aa
+ax2.plot(cF, [61.2,	63.3,	64.8,	64.6,	60.3], 'bx-') # poly  aa
+
+plt.show()
+# <codecell>
+
+
